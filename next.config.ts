665a38bc5173aa2a63e0ad5023db4195622f46a4
anrakotondrazah <anrakotondrazah@gmail.com>
2026-06-12 14:45:59 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,       // ← désactive l'optimisation (fix rapide)
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',      // ← autorise TOUS les domaines externes
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
